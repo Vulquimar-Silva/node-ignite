5cefef58a7aa74e5cc0eee0bae3500f3d07c8255
Vulquimar-Silva <vuquimar@hotmail.com>
2022-03-20 03:16:09 -0300

    update project FinAPI

--- finapi/index.js
+++ finapi/index.js
@@ -1,10 +1,57 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.post("/account", (req, res) => {
-  const cpf = req.body;
+app.use(express.json());
 
-})
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+
+  const { cpf } = req.headers;
+  
+  const customer = customers.find(
+    customer => customer.cpf === cpf
+  );
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+    return next();
+
+};
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    customer => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists" });
+  }
+
+  customers.push({
+    id: uuidv4(),
+    cpf,
+    name,
+    statement: []
+  });
+
+  return res.status(201).send();
+
+});
+
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  
+  return res.json(customer.statement);
+
+});
 
 app.listen(3333);
+console.log('🚀 Server started on port 3333!');
